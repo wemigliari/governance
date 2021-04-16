18fda417cf0ae2ec91aa2a8317cac70cbaee5b51
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-04-16 22:20:30 +0200

    Add files via upload

--- moradores_rua.R
+++ moradores_rua.R
@@ -9,15 +9,15 @@ rua_region <- data.frame(rua_region)
 plot_ly(rua_region, x = rua_region$ANO, y = rua_region$BRASIL, 
         type = 'scatter', mode = 'lines', 
         name = "Brasil",
-        color = I("black"))%>%
+        color = I("orange"))%>%
   add_trace(rua_region$NORTE,y = rua_region$NORTE, name = "Norte",
-            color = I("#768976"),type = 'scatter', mode = 'lines')%>%
+            color = I("black"),type = 'scatter', mode = 'lines')%>%
   add_trace(y = rua_region$NORDESTE, name = "Nordeste",
-            color = I("#4eb14e"),type = 'scatter', mode = 'lines')%>%
+            color = I("gold"),type = 'scatter', mode = 'lines')%>%
   add_trace(y = rua_region$SUDESTE, name = "Sudeste",
-            color = I("#007600"),type = 'scatter', mode = 'lines')%>%
+            color = I("coral"),type = 'scatter', mode = 'lines')%>%
   add_trace(y = rua_region$SUL, name = "Sul",
-            color = I("#1de21d"),type = 'scatter', mode = 'lines')%>%
+            color = I("yellow"),type = 'scatter', mode = 'lines')%>%
   add_trace(y = rua_region$CENTRO, name = "Centro",
             color = I("darkgray"),type = 'scatter', mode = 'lines')%>%
   layout(showlegend = TRUE)
@@ -29,11 +29,11 @@ rua_capitals <- read_xlsx("/Users/wemigliari/Documents/R/tabelas/brasil_popula_r
 
 plot_ly(rua_capitals, x =rua_capitals$ANO, y = rua_capitals$`PEQUENO I`, type = 'bar', 
         name = 'Pequeno I', color = I("black"))%>%
-  add_trace(y = rua_capitals$`PEQUENO II`, name = 'Pequeno II', color = I("#768976"))%>%
-  add_trace(y = rua_capitals$MEDIO, name = 'Médio', color = I("#4eb14e"))%>%
-  add_trace(y = rua_capitals$GRANDE, name = 'Grande', color = I("#007600"))%>%
-  add_trace(y = rua_capitals$METROPOLE, name = 'Metrópole', color = I("#1de21d"))%>%
-  add_trace(y = rua_capitals$TOTAL, name = 'Total', color = I("darkgray"))%>%
+  add_trace(y = rua_capitals$`PEQUENO II`, name = 'Pequeno II', color = I("gray"))%>%
+  add_trace(y = rua_capitals$MEDIO, name = 'Médio', color = I("coral"))%>%
+  add_trace(y = rua_capitals$GRANDE, name = 'Grande', color = I("gold"))%>%
+  add_trace(y = rua_capitals$METROPOLE, name = 'Metrópole', color = I("yellow"))%>%
+  add_trace(y = rua_capitals$TOTAL, name = 'Total', color = I("orange"))%>%
   layout(title = "",
           xaxis = list(title = ""),
           yaxis = list(title = ""), barmode="stack")
@@ -44,9 +44,9 @@ rua_pop <- read_xlsx("/Users/wemigliari/Documents/R/tabelas/brasil_popula_rua.xl
                       sheet = "Population")
 
 plot_ly(rua_pop, x = rua_pop$Ano, y =  rua_pop$`Censo Suas`, type = 'scatter', 
-        mode = 'markers+lines', color = I("red"), name = "Censo SUAS")%>%
+        mode = 'markers+lines', color = I("orange"), name = "Censo SUAS")%>%
   add_markers(x = rua_pop$Ano, rua_pop$`Cadastro Único`, type = 'scatter', 
-              mode = 'markers', color = I("darkgreen"), name = "Cadastro Único",
+              mode = 'markers', color = I("orange"), name = "Cadastro Único",
               marker = list(size = rua_pop$`Cadastro Único`/1500 , opacity = 0.4))
   
   
