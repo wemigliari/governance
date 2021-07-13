519ef8b7a6d4652fce4a624591e47742c09dbe6e
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-07-13 22:23:34 +0200

    Add files via upload

--- vale_data.R
+++ vale_data.R
@@ -62,6 +62,7 @@ p
 
 #####Graph2
 
+library(zoo)
 colors <- c("Valor Final" = "red", "Maior Valor" = "gold", "Menor Valor" = "#ffa500")
 
 
@@ -72,6 +73,7 @@ p2 <- ggplot(shares2, aes(x=Year)) +
   xlab("") +
   theme_ipsum() +
   theme(plot.caption = element_text(size = 10)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(title="Gr??fico 2. Varia????o do Valor das A????es da Vale S/A na Bolsa de Nova Iorque",
        y = "Varia????o das A????es (US$)", caption = "Fonte: Nasdaq. Elaborado por Migliari, W. (2021).",
        color = "") +
@@ -90,13 +92,14 @@ p2 <- ggplot(shares2, aes(x=Year)) +
                linetype="dashed", size = 0.5)
 p2
 
-#####Graph2
+#### scale_x_yearmon(format="%Y %m", n=20) + Retirada a linha, porque parece desnecess??ria.
+
+#####Graph3
 
 library(ggtext)
 
 p3 <- ggplot(shares2, aes(x=Year, y= Volume/1000000)) +
   geom_line(color="red") + 
-
   annotate("rect", xmin = as.Date("2015-01-01"), xmax = as.Date("2021-06-21"), ymin = 0, ymax = 150,
            alpha = .2) +
   xlab("") +
@@ -119,7 +122,83 @@ p3 <- ggplot(shares2, aes(x=Year, y= Volume/1000000)) +
 p3
 
 
-#####Graph4
+#####Graph4 - Correla????es
 
-library("writexl")
-write_xlsx(shares2, "/Users/wemigliari/Documents/R/Tabelas/vale_shares.xlsx")
+library(dplyr)
+library(lubridate)
+shares3 <- shares2[,-c(1,3:5)]
+
+### Soma do volume de a????es de todos os dias por ano - interessante, mas n??o serve, pois o interessante ?? uma m??dia anual
+shares4 <- shares3 %>%                #### https://stackoverflow.com/questions/63271525/summing-rows-by-month-over-years-in-r
+  mutate(Year = year(Year)) %>% 
+  group_by(Year) %>% 
+  filter(between(Year, 2011, 2021)) %>% 
+  summarise(Volume = sum(Volume, rm.na =TRUE))
+
+### M??dia anual do volume de a????es - o ideal, pois consigo analisar s?? a m??dia anual do volume operado
+
+shares5 <- shares3 %>% 
+  mutate(Year = year(Year)) %>% 
+  group_by(Year) %>% 
+  filter(between(Year, 2011, 2021)) %>% 
+  summarise(Volume = mean(Volume, rm.na =TRUE))
+
+
+
+ggplot(shares5, aes(x=Year, y= Volume/1000000)) +
+  geom_line(color="red")
+
+#####
+
+
+inter_demand <- read_xlsx("/Users/wemigliari/Documents/R/Tabelas/commodities_quantity_wto.xlsx",
+                          col_types = c("numeric", "numeric"))
+inter_demand <- inter_demand[-c(1:21),]
+
+shares_6 <- shares5[-c(10:11), -c(1)]
+
+table <- cbind(inter_demand, shares_6)
+
+
+color_sd <- c("Ferro Comprado" = "gold", "Volume Operado" = "#c47f00")
+
+ggplot(table, aes(x=Year)) +
+  geom_line(aes(y = iron_ore_consumption/1000000, color = "Ferro Comprado"), size = 1, linetype = "F1") + 
+  geom_line(aes(y = Volume/1000000, color = "Volume Operado"), size = 1, linetype = "longdash") +
+  xlab("") +
+  theme_ipsum() +
+  theme(plot.caption = element_text(size = 10)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(title="Gr??fico 4. M??dia Anual do Volume das A????es da Vale S/A na Bolsa de Nova Iorque e Ferro Comprado",
+       y = "A????es (milh??o) e Ferro (ton. m??tricas)", caption = "Fonte: Nasdaq. Elaborado por Migliari, W. (2021).",
+       color = "") +
+  labs(caption = "Fonte: Nasdaq. Elaborado por Migliari, W. (2021).",
+      color = "") +
+  scale_color_manual(values = color_sd) +
+  scale_x_continuous(breaks = c(2011:2019)) +
+  scale_y_continuous(
+    
+    # Features of the first axis
+    name = "Volume de A????es (milh??es)",
+    
+    # Add a second axis and specify its features
+    sec.axis = sec_axis( trans=~.*10, name="Ferro (mil ton. m??tricas)")
+  ) +
+  
+  annotate(geom="text", x=2014.94, y=10, label="Rompimento da Barragem do Fund??o, Mariana", 
+           angle = 90,
+           size=2,
+           color = "#3E3E3E") +
+  annotate(geom="text", x=2018.95, y=10, label="Rompimento da Barragem da Mina C??rrego do Feij??o, Brumadinho", 
+           angle = 90,
+           size=2,
+           color = "#3E3E3E")+
+  annotate("rect", xmin = 2015, xmax = 2019, ymin = 0, ymax = 30,
+           alpha = .2) +
+  geom_vline(xintercept= 2015, linetype="dotted", color = "red")+
+  geom_vline(xintercept= 2019, linetype="dotted", color = "red")
+
+  
+
+
+
