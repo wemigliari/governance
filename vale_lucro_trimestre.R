d9fb7d499d2d9277ff562b55d650676df1280756
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-07-13 22:24:58 +0200

    Add files via upload

--- vale_lucro_trimestre.R
+++ vale_lucro_trimestre.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(readxl)
 library(formattable)
+library(kable)
 
 lucros <- read_xlsx("/Users/wemigliari/Documents/R/tabelas/vale_lucro_trimestre.xlsx")
 lucros <- data.frame(lucros)
@@ -8,7 +9,13 @@ lucros <- data.frame(lucros)
 colnames(lucros)[3] <- "Data do Balanço"
 colnames(lucros)[4] <- "Lucro Líquido Ajustado pelo IPCA (Bilhões de Reais)"
 
-class(lucros)
 
-formattable(lucros, list(`Lucro Líquido Ajustado pelo IPCA (Bilhões de Reais)` =
-                           color_bar("steeblue", proportion)))
+customGreen <- c("#00c400", "#14ff14", "#89ff89", "#b1ffb1", "#ebffeb")
+
+
+formattable(lucros, digits = 5, 
+            align = c("l","l", "l", "r"),
+            format = "f", list(
+                           `Empresa` = formatter("span", style = ~ style(color = "grey",font.weight = "bold")), 
+                           `Lucro Líquido Ajustado pelo IPCA (Bilhões de Reais)`= color_bar(customGreen)
+                           ))
